a0ad1c09cd0017e3480a2ffd0f078343c534831f
Muhammad Nizar <76721471+nizarhazmi@users.noreply.github.com>
2021-07-05 09:18:16 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+function randomNumber(min=0,max=1){
+  if(min>=max) throw "Inavlid min value"; max -= min;
+  return Math.round(Math.random()*max)+min;
+}
+function randomBasicQuestion() {
+  let firstNumber = randomNumber(1,200),
+    secondNumber = randomNumber(1,100),
+    operation = ["+", "-"][randomNumber()];
+  if (operation === "+") {
+    var answer = firstNumber + secondNumber;
+  } else if (operation == "-") {
+    var answer = firstNumber - secondNumber;
+  }
+  return {
+    question: `${firstNumber} ${operation} ${secondNumber}`,
+    answer: answer
+  };
+}
+function randomComplexQuestion() {
+  let firstNumber = randomNumber(3,12),
+    secondNumber = randomNumber(3,12);
+  return {
+    question: `${firstNumber} x ${secondNumber}`,
+    answer: firstNumber * secondNumber
+  };
+}
+function randomRandomQuestion() {
+  return [randomBasicQuestion(), randomComplexQuestion()][randomNumber()];
+}
+
+var 
+userQuestion = randomRandomQuestion(),
+questionHTML = document.querySelector("span.question"),
+answerHTML = document.querySelector("input[type=number]"),
+container = document.querySelector("div.container");
+
+function updateQestion() {
+  userQuestion = randomRandomQuestion();
+ questionHTML.innerHTML = userQuestion.question;
+}
+
+updateQestion()
+
+answerHTML.onchange = () => {
+  var isCorrect = answerHTML.value == userQuestion.answer;
+  if(isCorrect){
+    updateQestion(); answerHTML.value = ""; 
+    container.style.animation = "correct .6s 1";
+  } else {
+    container.style.animation = "shake .8s 1";
+  }
+}
+container.onanimationend = () => {
+  container.style.animation = ""
+}
