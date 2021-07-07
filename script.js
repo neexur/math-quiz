9a2a577c83012ea2e0c7f7d51276701065325085
Muhammad Nizar Hazmi <76721471+nizarhazmi@users.noreply.github.com>
2021-07-07 21:11:14 +0800

    Bug fixes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function randomNumber(min=0,max=1){
-  if(min>=max) throw "Inavlid min value"; max -= min;
-  return Math.round(Math.random()*max)+min;
+// Pick random number
+function randomNumber(min = 0, max = 1) {
+  if (min >= max) throw "Inavlid min value"; max -= min;
+  return Math.round(Math.random() * max) + min;
 }
+
+// Create random basic(+,-) question
 function randomBasicQuestion() {
-  let firstNumber = randomNumber(1,200),
-    secondNumber = randomNumber(1,100),
+  let firstNumber = randomNumber(1, 200),
+    secondNumber = randomNumber(1, 100),
     operation = ["+", "-"][randomNumber()];
   if (operation === "+") {
     var answer = firstNumber + secondNumber;
@@ -16,87 +19,111 @@ function randomBasicQuestion() {
     answer: answer
   };
 }
+
+// Create random multiplication question
 function randomComplexQuestion() {
-  let firstNumber = randomNumber(3,12),
-    secondNumber = randomNumber(3,12);
+  let firstNumber = randomNumber(3, 12),
+    secondNumber = randomNumber(3, 12);
   return {
     question: `${firstNumber} x ${secondNumber}`,
     answer: firstNumber * secondNumber
   };
 }
+
+// Return random (basic or multiplication) question
 function randomRandomQuestion() {
   return [randomBasicQuestion(), randomComplexQuestion()][randomNumber()];
 }
 
-var 
-userQuestion = randomRandomQuestion(),
-questionHTML = document.querySelector("span.question"),
-answerHTML = document.querySelector("input[type=number]"),
-correctHTML = document.querySelector("span.correct"),
-wrongHTML = document.querySelector("span.wrong"),
-streakHTML = document.querySelector("span.streak"),
-bestStreakHTML = document.querySelector("span.best-streak"),
-answeredHTML = document.querySelector("span.answered"),
-allButtonsHTML = document.querySelectorAll("a,button"),
-deleteButtonHTML = document.querySelector("div.record button"),
-scoreHTML = document.querySelector("span.score"),
-container = document.querySelector("div.container"),
-records = {
-  score: 0,streak: 0,bestStreak: 0,
-  correct: 0,wrong: 0,answered: 0,
-};
+var userQuestion = randomRandomQuestion(),
+  questionHTML = document.querySelector("span.question"),
+  answerHTML = document.querySelector("input[type=number]"),
+  correctHTML = document.querySelector("span.correct"),
+  wrongHTML = document.querySelector("span.wrong"),
+  streakHTML = document.querySelector("span.streak"),
+  bestStreakHTML = document.querySelector("span.best-streak"),
+  answeredHTML = document.querySelector("span.answered"),
+  daysHTML = document.querySelector("span.days"),
+  allButtonsHTML = document.querySelectorAll("a,button"),
+  deleteButtonHTML = document.querySelector("div.record button"),
+  scoreHTML = document.querySelector("span.score"),
+  container = document.querySelector("div.container"),
+  storage = window.localStorage,
+  records = {
+    score: 0, streak: 0, bestStreak: 0,
+    correct: 0, wrong: 0, answered: 0
+  };
 
+// Give some Haptic feedback
 function vibrate(time) {
-  window.navigator.vibrate(time)
+  window.navigator.vibrate(time);
 }
+
+// Format to human readable (today, yesterday, n days before)
+function formatDay(day) {
+  if(day==0) return "Today";
+  if(day==1) return "Yesterday";
+  if(day>1) return (day + " days ago");
+}
+
+// Move to next question
 function updateQestion() {
   userQuestion = randomRandomQuestion();
   questionHTML.innerHTML = userQuestion.question;
 }
+
+// Change info after record have been manupilate
 function updateInfo() {
-  var savedrecords = window.localStorage;
-  var msg = "No record"
-  if(savedrecords) {
-    correctHTML.innerHTML = savedrecords.correct || msg;
-    wrongHTML.innerHTML = savedrecords.wrong || msg;
-    streakHTML.innerHTML = savedrecords.streak || msg;
-    bestStreakHTML.innerHTML = savedrecords.bestStreak || msg;
-    answeredHTML.innerHTML = savedrecords.answered || msg;
-    scoreHTML.innerHTML = (Math.round(savedrecords.score) || 0)+"%";
-    return
-  }
-}
-function saveRecords () {
-  Object.keys(records).forEach(record => {
-    window.localStorage.setItem(record,records[record])
-  })
+  var msg = "No record";
+  correctHTML.innerHTML = records.correct || msg;
+  wrongHTML.innerHTML = records.wrong || msg;
+  streakHTML.innerHTML = records.streak || msg;
+  bestStreakHTML.innerHTML = records.bestStreak || msg;
+  answeredHTML.innerHTML = records.answered || msg;
+  scoreHTML.innerHTML = (records.score || 0) + "%";
 }
 
-updateQestion(); updateInfo();
+// Extract (pass) records from localStorage to records
+function updateRecords() {
+  Object.keys(records).forEach((record) => {
+    records[record] = Number(storage.getItem(record));
+  });
+}
+
+// Save updated records to localStorage
+function saveRecords() {
+  Object.keys(records).forEach((record) => {
+    window.localStorage.setItem(record, records[record]);
+  });
+}
+
+updateRecords(); 
+updateInfo();
+updateQestion();
 
 answerHTML.onchange = () => {
   var isCorrect = answerHTML.value == userQuestion.answer;
-  if(isCorrect){
-    updateQestion(); answerHTML.value = ""; 
+  if (isCorrect) {
+    updateQestion(); answerHTML.value = "";
     container.style.animation = "correct .6s 1";
-    records.correct++; records.streak++; vibrate(100)
-    if(records.streak>records.bestStreak) 
-      records.bestStreak=records.streak
+    records.correct++; records.streak++; vibrate(100);
+    if (records.streak > records.bestStreak)
+      records.bestStreak = records.streak;
   } else {
     container.style.animation = "shake .8s 1";
-    records.streak = 0; records.wrong++; vibrate(400)
-  };
-  records.answered = records.correct+records.wrong; 
-  records.score=records.correct/records.answered*100;
-  saveRecords(); updateInfo(); 
-}
+    records.wrong++; records.streak = 0; vibrate(800);
+  }
+  records.answered = records.correct + records.wrong;
+  records.score = Math.round((records.correct / records.answered) * 100);
+  saveRecords(); updateInfo();
+};
 container.onanimationend = () => {
-  container.style.animation = ""
-}
-allButtonsHTML.forEach( button => {
-  button.onclick = () => vibrate(100)
-})
+  container.style.animation = "";
+};
+allButtonsHTML.forEach((button) => {
+  button.onclick = () => vibrate(100);
+});
 deleteButtonHTML.onclick = () => {
-  window.localStorage.clear()
-  updateInfo()
-}
+  window.localStorage.clear();
+  updateRecords(); updateInfo();
+};
